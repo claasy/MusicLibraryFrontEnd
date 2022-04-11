4ce7dae499d7da26aab86e6f5fd7f4e8e9c7584f
Amanda <aclaas522@gmail.com>
2022-04-11 16:45:23 -0500

    Created AddSong Component and Instantiated it on Apps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
+import AddSongForm from './Components/AddSong/AddSongForm';
 
 
 function App() {
 
   const [entries, setEntries] = useState([{songTitle: "In My Life", artist: "The Beatles", albumName: "Rubber Soul", genre: "Rock'n Roll", releaseDate: "12/03/1965"}])
 
+  function addNewSong(entry){
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayMusic parentEntries={entries} />
+      <AddSongForm addNewSongProperty={addNewSong} />
+
     </div>
   );
 }
